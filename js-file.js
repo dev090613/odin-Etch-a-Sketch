2651695f450cc96cd8df1b912c2e4bf4aa54c03b
kokoro <128305501+dev090613@users.noreply.github.com>
2025-07-29 13:30:15 +0900

    feature: add button for generating grid squares
    사용자의 입력을 받는 버튼을 추가하였다
    해당 버튼에 입력된 숫자 만큼 한 변에 들어갈 grid가 결정된다.
    입력을 받을 때 발생할 수 있는 여러 예외들을 처리하였다.

--- js-file.js
+++ js-file.js
@@ -1,15 +1,49 @@
 const container = document.querySelector(".container")
 const gridSize = 16
-
-for (let i=0; i < gridSize * gridSize; i++) {
-  const gridItem = document.createElement("div")
-  gridItem.classList.add("grid-item")
-  container.appendChild(gridItem);
-};
+const WIDTH = 960;
+const HEIGHT = 960;
 
 container.addEventListener("mouseover", (e) => {
   const target = e.target.closest(".grid-item")
   if (target) {
+    // console.log(target.style)
     target.classList.add("grid-hovered")
   }
 })
+
+const btn = document.querySelector("#btn")
+btn.addEventListener("click", () => {
+  const userInput 
+    = prompt("한 변에 들어갈 그리드의 갯수를 입력해주세요?")
+
+  if ((userInput ==="") || (userInput === null)) {
+    alert("1 이상 100 이하의 숫자를 입력해주세요.")
+    return;
+  }
+  const gridSize = parseInt(userInput);
+  if (gridSize > 100) {
+    alert("100 이하의 숫자를 입력해주세요");
+    return;
+  } else if (gridSize < 1) {
+    alert("1 이상의 숫자를 입력해주세요.")
+    return;
+  }
+
+  drawGrid(gridSize);
+})
+
+function drawGrid(gridSize) {
+  container.innerHTML = ''
+  const basis = (100 / gridSize)
+  const height = (WIDTH / gridSize)
+  
+  for (let i=0; i < gridSize * gridSize; i++) {
+    const gridItem = document.createElement("div")
+    gridItem.classList.add("grid-item")
+    gridItem.style.flexBasis = `${basis}%`
+    gridItem.style.height = `${height}px`
+    container.appendChild(gridItem);
+  };
+}
+
+drawGrid(16);
