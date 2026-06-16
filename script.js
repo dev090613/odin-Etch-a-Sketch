5d73796585269d3647c10e64b27c4317f83ca264
dev090613 <128305501+dev090613@users.noreply.github.com>
2026-06-16 15:15:04 +0900

    edit index.html

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ for (let i = 0; i < num; i++) {
   for (let j = 0; j < num; j++) {
     const div = document.createElement("div")
     div.style.minWidth = (`calc(100vw / ${num})`)
-    div.style.aspectRatio = "1 / 1";
 
     div.classList.add("item")
     container.appendChild(div)
